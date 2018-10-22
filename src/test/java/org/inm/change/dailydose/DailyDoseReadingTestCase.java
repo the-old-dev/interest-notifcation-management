4a700c2f6f58203dec3074514e9fb5f7c19f1e79
the-old-dev <gskiker@gmail.com>
2018-10-22 20:05:14 +0200

    DailyDose Chunk tested

--- src/test/java/org/inm/change/dailydose/DailyDoseReadingTestCase.java
+++ src/test/java/org/inm/change/dailydose/DailyDoseReadingTestCase.java
@@ -10,13 +10,35 @@ import org.inm.interests.Interest;
 public class DailyDoseReadingTestCase {
 
 	@Test
-	public void testOffersChangeDetection() throws Exception {
+	public void testOffersChangeDetectionChunk() throws Exception {
 
 		// define variables
 		DailyDoseOffersChangeDetector changeDetector = new DailyDoseOffersChangeDetector();
-
+		
+		// test chunk
 		Iterator<Interest> interests = changeDetector.getNextChunk();
-
 		Assert.assertTrue(interests.hasNext());
+		
+		while (interests.hasNext()) {
+			Interest interest = interests.next();
+			Assert.assertNotNull(interest);
+			Assert.assertNotNull(interest.getCategory());
+			Assert.assertNotNull(interest.getSubCategory());
+			Assert.assertNotNull(interest.getTitle());
+			Assert.assertNotNull(interest.getLastUpdated());
+			Assert.assertNotNull(interest.getUrl());
+			Assert.assertNotNull(interest.getDetails());
+			Assert.assertNotNull(interest.getLastUpdated());
+			Assert.assertNotNull(interest.getDetails());
+			Assert.assertNotNull(interest.getDetails().get("price"));
+			Assert.assertNotNull(interest.getDetails().get("description"));
+			Assert.assertNotNull(interest.getDetails().get("date"));
+			Assert.assertNotNull(interest.getDetails().get("location"));
+			Assert.assertNotNull(interest.getDetails().get("image"));
+			Assert.assertNotNull(interest.getDetails().get("imageType"));
+		}
+		
 	}
+	
+	
 }
